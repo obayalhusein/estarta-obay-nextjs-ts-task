11cd1b680148ab172c317146b24dcf475fcd6ab8
obayalhusein <obayalhusein@gmail.com>
2022-11-14 02:19:48 +0300

    add material ui / material ui icons / material ui datatable

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
+  <Head>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
+  </Head>
   return <Component {...pageProps} />
 }
